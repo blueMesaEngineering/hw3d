c57f4eb015a874607984c70996aaa5d35a537d67
ukrgr <ukrgr@DESKTOP-GGUUT7R>
2023-02-04 01:19:10 +0300

    Add project files.

---
+++ hw3d/WinMain.cpp
@@ -0,0 +1,66 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR	  lpCmdLine,
+	int		  nCmdShow )
+{
+	const auto pClassName = L"hw3dbutts";  // NDGuthrie 20230203 - Had to add the 'L' to the beginning of the string as per the instructions here: https://stackoverflow.com/questions/60044576/e0513-a-value-of-type-const-char-cannot-be-assigned-to-an-entity-of-type-lp
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx( &wc );
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		L"Happy Hard Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr
+	);
+	ShowWindow(hWnd, SW_SHOW);
+
+	// message pump
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+}
