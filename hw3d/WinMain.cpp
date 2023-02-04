877a4b319e8c9c3f4694efcf552241300d872cbc
bluemesaengineering <nathan@bluemesaengineering.com>
2023-02-05 02:10:52 +0300

    Testing WM_CHAR and WM_LBUTTONDOWN.

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include "WindowsMessageMap.h"
+#include <sstream>
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -10,6 +11,33 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CLOSE:
 		PostQuitMessage(69);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == 'F')
+		{
+			SetWindowTextA(hWnd, "Respects");
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam == 'F')
+		{
+			SetWindowTextA(hWnd, "Dangerfield");
+		}
+		break;
+	case WM_CHAR:
+		{
+			static std::string title;
+			title.push_back((char)wParam);
+			SetWindowTextA(hWnd, title.c_str());
+		}
+		break;
+	case WM_LBUTTONDOWN:
+		{
+			POINTS pt = MAKEPOINTS(lParam);
+			std::ostringstream oss;
+			oss << "(" << pt.x << "," << pt.y << ")";
+			SetWindowTextA(hWnd, oss.str().c_str());
+		}
 		break;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
