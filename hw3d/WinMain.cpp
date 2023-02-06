d4fe79f41abe1d7dbbe3cae864118d49067b3562
bluemesaengineering <nathan@bluemesaengineering.com>
2023-02-06 19:04:32 +0300

    Added try-catch exception handler block to WinMain.cpp.

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -8,24 +8,41 @@ int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
 	LPSTR	  lpCmdLine,
-	int		  nCmdShow )
+	int		  nCmdShow)
 {
-	Window wnd(800, 300, L"Donkey Fart Box With Noodles!!!");
 
-	MSG msg;
-	BOOL gResult;
-	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	try
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
+		Window wnd(800, 300, L"Donkey Fart Box With Noodles!!!");
 
-	if (gResult == -1)
-	{
-		return -1;
+		MSG msg;
+		BOOL gResult;
+		while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (gResult == -1)
+		{
+			return -1;
+		}
+		else
+		{
+			return msg.wParam;
+		}
 	}
-	else
+	catch(const ChiliException& e)
 	{
-		return msg.wParam;
+		MessageBox(nullptr, (LPCWSTR)e.what(), (LPCWSTR)e.GetType(), MB_OK | MB_ICONEXCLAMATION)
 	}
+	catch (const std::exception& e)
+	{
+		MessageBox(nullptr, (LPCWSTR)e.what(), L"Standard Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (...)
+	{
+		MessageBox(nullptr, L"No details available", L"Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	return -1;
 }
