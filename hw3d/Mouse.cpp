cd2374e201b876247ab54ba364c6ed2310bb4c26
bluemesaengineering <nathan@bluemesaengineering.com>
2023-02-23 00:34:31 +0300

    Adding Mouse.h and Mouse.cpp for mouse input capture.

---
+++ hw3d/Mouse.cpp
@@ -0,0 +1,54 @@
+#include "Mouse.h"
+
+std::pair<int, int> Mouse::GetPos() const noexcept
+{
+	return { x, y };
+}
+
+int Mouse::GetPosX() const noexcept
+{
+	return x;
+}
+
+int Mouse::GetPosY() const noexcept
+{
+	return y;
+}
+
+bool Mouse::LeftIsPressed() const noexcept
+{
+	return leftIsPressed;
+}
+
+bool Mouse::RightIsPressed() const noexcept
+{
+	return rightIsPressed;
+}
+
+Mouse::Event Mouse::Read() noexcept
+{
+	if (buffer.size() > 0u)
+	{
+		Mouse::Event e = buffer.front();
+		buffer.pop();
+		return e;
+	}
+	else
+	{
+		return Mouse::Event();
+	}
+}
+
+void Mouse::Flush() noexcept
+{
+	buffer = std::queue<Event>();
+}
+
+void Mouse::OnMouseMove(int newX, int newY) noexcept
+{
+	x = newX;
+	y = newY;
+	buffer.push(Mouse::Event(Mouse::Event::Type::Move, *this));
+	TrimBuffer();
+}
+
