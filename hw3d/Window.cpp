48471ab15dbd43c806dce0886835b22122366e5f
bluemesaengineering <nathan@bluemesaengineering.com>
2023-02-26 23:11:31 +0300

    Fixed init variable values for Window.cpp constructor.  Fixed WM_MOUSEMOVE case with const modifier and break statement.

--- hw3d/Window.cpp
+++ hw3d/Window.cpp
@@ -43,6 +43,9 @@ HINSTANCE Window::WindowClass::GetInstance() noexcept
 
 // Window Stuff
 Window::Window(int width, int height, const wchar_t* name)
+	:
+	width(width),
+	height(height)
 {
 	// calculate window size based on desired client region size
 	RECT wr;
@@ -159,8 +162,9 @@ LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noe
 	/******************** MOUSE MESSAGES ********************/
 	case WM_MOUSEMOVE:
 	{
-		POINTS pt = MAKEPOINTS(lParam);
+		const POINTS pt = MAKEPOINTS(lParam);
 		mouse.OnMouseMove(pt.x, pt.y);
+		break;
 	}
 	case WM_LBUTTONDOWN:
 	{
