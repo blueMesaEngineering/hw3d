5e6738d0b995f7e8d6a7f5351de15edc18db9fc8
bluemesaengineering <nathan@bluemesaengineering.com>
2023-03-04 02:48:00 +0300

    Built basic Graphics class.

--- hw3d/Graphics.h
+++ hw3d/Graphics.h
@@ -6,4 +6,11 @@ class Graphics
 {
 public:
 	Graphics(HWND hWnd);
+	Graphics(const Graphics&) = delete;
+	Graphics& operator=(const Graphics&) = delete;
+	~Graphics();
+private:
+	ID3D11Device* pDevice = nullptr;
+	IDXGISwapChain* pSwap = nullptr;
+	ID3D11DeviceContext* pContext = nullptr;
 };
