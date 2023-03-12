7708e4797f1d772ca10e17b03847625452c10ee9
bluemesaengineering <nathan@bluemesaengineering.com>
2023-03-12 04:19:22 +0300

    Edited Graphics class to pull in info from DxgiInfoManager for error reporting.

--- hw3d/Graphics.cpp
+++ hw3d/Graphics.cpp
@@ -4,8 +4,19 @@
 
 #pragma comment(lib, "d3d11.lib")
 
-#define GFX_THROW_FAILED(hrcall) if(FAILED(hr = (hrcall))) throw Graphics::HrException(__LINE__, __FILE__, hr)
+// Graphics exception checking/throwing macros (some with dxgi infos)
+#define GFX_EXCEPT_NOINFO(hr) Graphics::HrException(__LINE__, __FILE__, (hr))
+#define GFX_THROW_NOINFO(hrcall) if(FAILED(hr = (hrcall))) throw Graphics::HrException(__LINE__, __FILE__, hr)
+
+#ifndef NDEBUG
+#define GFX_EXCEPT(hr) Graphics::HrException(__LINE__, __FILE__, (hr), infoManager.GetMessages())
+#define GFX_THROW_INFO(hrcall) infoManager.Set(); if(FAILED(hr = (hrcall))) throw GFX_EXCEPT(hr)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__, __FILE__, (hr), infoManager.GetMessages())
+#else
+#define GFX_EXCEPT(hr) Graphics::HrException(__LINE__, __FILE__, (hr))
+#define GFX_THROW_INFO(hrcall) GFX_THROW_NOINFO(hrcall)
 #define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__, __FILE__, (hr))
+#endif // !1
 
 Graphics::Graphics(HWND hWnd)
 {
@@ -26,11 +37,17 @@ Graphics::Graphics(HWND hWnd)
 	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 	sd.Flags = 0;
 
+	UINT swapCreateFlags = 0u;
+#ifndef NDEBUG
+	swapCreateFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif // !NDEBUG
+
+
 	// For checking results of d3d functions
 	HRESULT hr;
 
 	// Create device and front/back buffers, and swap chain and rendering context
-	GFX_THROW_FAILED(D3D11CreateDeviceAndSwapChain(
+	GFX_THROW_INFO(D3D11CreateDeviceAndSwapChain(
 		nullptr,
 		D3D_DRIVER_TYPE_HARDWARE,
 		nullptr,
@@ -46,8 +63,8 @@ Graphics::Graphics(HWND hWnd)
 	));
 	// Gain access to texture subresource in swap chain (back buffer)
 	ID3D11Resource* pBackBuffer = nullptr;
-	GFX_THROW_FAILED(pSwap->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void**>(&pBackBuffer)));
-	GFX_THROW_FAILED(pDevice->CreateRenderTargetView(
+	GFX_THROW_INFO(pSwap->GetBuffer(0, __uuidof(ID3D11Resource), reinterpret_cast<void**>(&pBackBuffer)));
+	GFX_THROW_INFO(pDevice->CreateRenderTargetView(
 		pBackBuffer,
 		nullptr,
 		&pTarget
@@ -86,7 +103,7 @@ void Graphics::EndFrame()
 		}
 		else
 		{
-			GFX_THROW_FAILED(hr);
+			throw GFX_EXCEPT(hr);
 		}
 	}
 }
@@ -98,11 +115,23 @@ void Graphics::ClearBuffer(float red, float green, float blue) noexcept
 }
 
 // Graphics Exception Stuff
-Graphics::HrException::HrException(int line, const char * file, HRESULT hr) noexcept
+Graphics::HrException::HrException(int line, const char * file, HRESULT hr, std::vector<std::string> infoMsgs) noexcept
 	:
 	Exception(line, file),
 	hr(hr)
-{}
+{
+	// Join all info messages with newlines into one string
+	for (const auto& m : infoMsgs)
+	{
+		info += m;
+		info.push_back('\n');
+	}
+	// Remove final newline if exists
+	if (!info.empty())
+	{
+		info.pop_back();
+	}
+}
 
 const char* Graphics::HrException::what() const noexcept
 {
@@ -111,8 +140,12 @@ const char* Graphics::HrException::what() const noexcept
 		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
 		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
 		<< "[Error String] " << GetErrorString() << std::endl
-		<< "[Description] " << GetErrorDescription() << std::endl
-		<< GetOriginString();
+		<< "[Description] " << GetErrorDescription() << std::endl;
+	if (!info.empty())
+	{
+		oss << "\n[Error info]\n" << GetErrorInfo() << std::endl << std::endl;
+	}
+	oss	<< GetOriginString();
 	whatBuffer = oss.str();
 	return whatBuffer.c_str();
 }
@@ -139,6 +172,11 @@ std::string Graphics::HrException::GetErrorDescription() const noexcept
 	std::wstring wbuf(buf);
 	std::string msg(wbuf.begin(), wbuf.end());
 	return msg;
+}
+
+std::string Graphics::HrException::GetErrorInfo() const noexcept
+{
+	return info;
 }
 
 const char* Graphics::DeviceRemovedException::GetType() const noexcept
