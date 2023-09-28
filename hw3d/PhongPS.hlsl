46bdf66ec3a2b52e07a2169b51af32f619e81bd2
Nathan <nathan@bluemesaengineering.com>
2023-09-28 16:22:37 +0300

    Need to work in camera's frame of reference for specular code to work. Corresponds to Chili Framework commit f0ac931f.

--- hw3d/PhongPS.hlsl
+++ hw3d/PhongPS.hlsl
@@ -27,10 +27,10 @@ float4 main(float3 worldPos : Position, float3 n : Normal) : SV_Target
 	// Diffuse intensity
 	const float3 diffuse = diffuseColor * diffuseIntensity * att * max(0.0f, dot(dirToL, n));
 	// Reflected light vector
-	const float3 w = n * dot(dirToL, n);
-	const float3 r = w * 2.0f - dirToL;
-	// Calculate specular intensity based on angle between viewing vector, narrow with power function
-	const float3 specular = (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f, dot(normalize(r), normalize(worldPos))), specularPower);
+	const float3 w = n * dot(vToL, n);
+	const float3 r = w * 2.0f - vToL;
+	// Calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
+	const float3 specular = (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f, dot(normalize(-r), normalize(worldPos))), specularPower );
 	// Final color
 	return float4(saturate((diffuse + ambient + specular) * materialColor), 1.0f);
 }
