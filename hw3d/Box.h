c18f46eec073311e5ad72776feabd1b3e17428a4
Nathan <nathan@bluemesaengineering.com>
2023-09-30 05:29:58 +0300

    Create a nice base for our test objects. Corresponds to Chili Framework commit f61cdfde.

--- hw3d/Box.h
+++ hw3d/Box.h
@@ -1,11 +1,11 @@
 #pragma once
-#include "DrawableBase.h"
+#include "TestObject.h"
 
-class Box : public DrawableBase<Box>
+class Box : public TestObject<Box>
 {
 public:
 	Box(
-		  Graphics& gfx
+		Graphics& gfx
 		, std::mt19937& rng
 		, std::uniform_real_distribution<float>& adist
 		, std::uniform_real_distribution<float>& ddist
@@ -13,24 +13,8 @@ public:
 		, std::uniform_real_distribution<float>& rdist
 		, std::uniform_real_distribution<float>& bdist
 		, DirectX::XMFLOAT3 material);
-	void Update(float dt) noexcept override;
 	DirectX::XMMATRIX GetTransformXM() const noexcept override;
 private:
-	// positional
-	float r;
-	float roll = 0.0f;
-	float pitch = 0.0f;
-	float yaw = 0.0f;
-	float theta;
-	float phi;
-	float chi;
-	// speed (delta/s)
-	float droll;
-	float dpitch;
-	float dyaw;
-	float dtheta;
-	float dphi;
-	float dchi;
 	// model transform
 	DirectX::XMFLOAT3X3 mt;
 };
