5e3db59913bcc335ff7e729fb9f50125a1e0cb0c
Nathan <nathan@bluemesaengineering.com>
2023-11-09 01:38:19 +0200

    Node parsing model system beginnings - part 2. Corresponds to Chili Framework commit 2a5d18ff.

--- hw3d/App.cpp
+++ hw3d/App.cpp
@@ -10,9 +10,6 @@
 #include "Surface.h"
 #include "GDIPlusManager.h"
 #include "imgui/imgui.h"
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
 
 namespace dx = DirectX;
 
@@ -23,101 +20,6 @@ App::App()
 	wnd(800, 600, L"The Donkey Fart Box - Now with RAMEN!"),
 	light(wnd.Gfx())
 {
-	class Factory
-
-	{
-	public:
-		Factory(Graphics& gfx)
-			:
-			gfx(gfx)
-		{}
-		std::unique_ptr<Drawable> operator()()
-		{
-			const DirectX::XMFLOAT3 mat = { cdist(rng), cdist(rng), cdist(rng) };
-
-			switch (sdist(rng))
-			{
-			case 0:
-				return std::make_unique<Box>(
-					gfx
-					, rng
-					, adist
-					, ddist
-					, odist
-					, rdist
-					, bdist
-					, mat
-				);
-			case 1:
-				return std::make_unique<Cylinder>(
-					gfx
-					, rng
-					, adist
-					, ddist
-					, odist
-					, rdist
-					, bdist
-					, tdist
-				);
-			case 2:
-				return std::make_unique<Pyramid>(
-					gfx
-					, rng
-					, adist
-					, ddist
-					, odist
-					, rdist
-					, tdist
-				);
-			case 3:
-				return std::make_unique<SkinnedBox>(
-					gfx
-					, rng
-					, adist
-					, ddist
-					, odist
-					, rdist
-				);
-			case 4:
-				return std::make_unique<AssTest>(
-					gfx
-					, rng
-					, adist
-					, ddist
-					, odist
-					, rdist
-					, mat
-					, 1.5f
-				);
-			default:
-				assert(false && "Impossible drawable option in factory");
-				return {};
-			}
-		}
-	private:
-		Graphics& gfx;
-		std::mt19937 rng{ std::random_device{}() };
-		std::uniform_int_distribution<int> sdist{ 0, 4 };
-		std::uniform_real_distribution<float> adist{ 0.0f, PI * 2.0f };
-		std::uniform_real_distribution<float> ddist{ 0.0f, PI * 0.5f };
-		std::uniform_real_distribution<float> odist{ 0.0f, PI * 0.08f };
-		std::uniform_real_distribution<float> rdist{ 6.0f, 20.0f };
-		std::uniform_real_distribution<float> bdist{ 0.4f, 3.0f };
-		std::uniform_real_distribution<float> cdist{ 0.0f, 1.0f };
-		std::uniform_int_distribution<int> tdist{ 3, 30 };
-	};
-
-	drawables.reserve(nDrawables);
-	std::generate_n(std::back_inserter(drawables), nDrawables, Factory{ wnd.Gfx() });
-	// Init box pointers for editing parameters
-	for (auto& pd : drawables)
-	{
-		if (auto pb = dynamic_cast<Box*>(pd.get()))
-		{
-			boxes.push_back(pb);
-		}
-	}
-
 	wnd.Gfx().SetProjection(dx::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
 }
 
@@ -128,95 +30,20 @@ void App::DoFrame()
 	wnd.Gfx().SetCamera(cam.GetMatrix());
 	light.Bind(wnd.Gfx(), cam.GetMatrix());
 
-	// Render geometry
-	for (auto& d : drawables)
-	{
-		d->Update(wnd.kbd.KeyIsPressed(VK_SPACE) ? 0.0f : dt);
-		d->Draw(wnd.Gfx());
-	}
+	nano.Draw(wnd.Gfx());
 	light.Draw(wnd.Gfx());
 
 	// ImGui windows
-	SpawnSimulationWindow();
 	cam.SpawnControlWindow();
 	light.SpawnControlWindow();
-	SpawnBoxWindowManagerWindow();
-	SpawnBoxWindows();
 
 	// Present
 	wnd.Gfx().EndFrame();
 }
 
-void App::SpawnSimulationWindow() noexcept
-{
-	if (ImGui::Begin("Simulation Speed"))
-	{
-		ImGui::SliderFloat("Speed Factor", &speed_factor, 0.0f, 6.0f, "%.4f");
-		ImGui::Text("%.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		ImGui::Text("Status: %s", wnd.kbd.KeyIsPressed(VK_SPACE) ? "PAUSED" : "RUNNING (Hold spacebar to pause)");
-	}
-	ImGui::End();
-}
-
-void App::SpawnBoxWindowManagerWindow() noexcept
-{
-	if (ImGui::Begin("Boxes"))
-	{
-		using namespace std::string_literals;
-		const auto preview = comboBoxIndex ? std::to_string(*comboBoxIndex) : "Choose a box..."s;
-
-		if (ImGui::BeginCombo("Box Number", preview.c_str()))
-		{
-			for (int i = 0; i < boxes.size(); i++)
-			{
-				const bool selected = comboBoxIndex == i;   // No, this is not a typo here.  In the Chili Framework code, 
-				//	there IS a pointer attached to comboBoxIndex, so there, it is
-				//  *comboBoxIndex.  However, due to a change in how <optional>
-				//  is implemented in C++ 20/22 (after C++ 17, in which the Chili
-				//	Framework is originally written), I was getting runtime errors
-				//  because <optional> was not allowed to be accessed when it was
-				//  still nullptr. <optional> line 186 was the place where the 
-				//	error was being thrown.  So after attempting other methods for
-				//	initializing comboBoxIndex before this point, I tried removing
-				//  the pointer and VIOLA - crisis averted.  Woot... 
-				//	NDG 2023.10.17:2011
-				if (ImGui::Selectable(std::to_string(i).c_str(), selected))
-				{
-					comboBoxIndex = i;
-				}
-				if (selected)
-				{
-					ImGui::SetItemDefaultFocus();
-				}
-			}
-			ImGui::EndCombo();
-		}
-		if (ImGui::Button("Spawn Control Window") && comboBoxIndex)
-		{
-			boxControlIds.insert(*comboBoxIndex);
-			comboBoxIndex.reset();
-		}
-	}
-	ImGui::End();
-}
-
-void App::SpawnBoxWindows() noexcept
-{
-	for (auto i = boxControlIds.begin(); i != boxControlIds.end();)
-	{
-		if (!boxes[*i]->SpawnControlWindow(*i, wnd.Gfx()))
-		{
-			i = boxControlIds.erase(i);
-		}
-		else
-		{
-			i++;
-		}
-	}
-}
-
 App::~App()
 {}
+
 
 int App::DoStuff()
 {
