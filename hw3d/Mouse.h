77edd5a8e042a486d42bc4642b9216e556f5d001
Nathan <nathan@bluemesaengineering.com>
2023-11-21 09:10:19 +0200

    Enable / disable raw input. Corresponds to Chili Framework commit  2a6d9115.

--- hw3d/Mouse.h
+++ hw3d/Mouse.h
@@ -82,6 +82,9 @@ public:
 		return buffer.empty();
 	}
 	void Flush() noexcept;
+	void EnableRaw() noexcept;
+	void DisableRaw() noexcept;
+	bool RawEnabled() const noexcept;
 private:
 	void OnMouseMove(int x, int y) noexcept;
 	void OnMouseLeave() noexcept;
@@ -104,6 +107,7 @@ private:
 	bool rightIsPressed = false;
 	bool isInWindow = false;
 	int wheelDeltaCarry = 0;
+	bool rawEnabled = false;
 	std::queue<Event> buffer;
 	std::queue<RawDelta> rawDeltaBuffer;
 };
--- hw3d/Window.cpp
+++ hw3d/Window.cpp
@@ -160,6 +160,11 @@ void Window::DisableCursor() noexcept
 	ConfineCursor();
 }
 
+bool Window::CursorEnabled() const noexcept
+{
+	return cursorEnabled;
+}
+
 std::optional<int> Window::ProcessMessages() noexcept
 {
 	MSG msg;
@@ -445,6 +450,10 @@ LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noe
 	/*************** RAW MOUSE MESSAGES ***************/
 	case WM_INPUT:
 	{
+		if (!mouse.RawEnabled())
+		{
+			break;
+		}
 		UINT size;
 		// First get the size of the input data
 		if (GetRawInputData(
