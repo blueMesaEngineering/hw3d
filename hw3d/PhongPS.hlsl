f7aa3fc3028878a26a098579a4a4aa0a6ebc7b0e
Nathan <nathan@bluemesaengineering.com>
2023-11-25 09:51:52 +0200

    Textured shading. Corresponds to Chili Frameworkd commit fc18dc8b.

--- hw3d/PhongPS.hlsl
+++ hw3d/PhongPS.hlsl
@@ -11,16 +11,21 @@ cbuffer LightCBuf
 
 cbuffer ObjectCBuf
 {
-	float3 materialColor;
 	float specularIntensity;
 	float specularPower;
-}
+	float padding[2];
+};
 
-float4 main(float3 worldPos : Position, float3 n : Normal) : SV_Target
+Texture2D tex;
+
+SamplerState splr;
+
+
+float4 main(float3 worldPos : Position, float3 n : Normal, float2 tc : Texcoord) : SV_Target
 {
 	// Fragment to light vector data
 	const float3 vToL = lightPos - worldPos;
-	const float distToL = length( vToL );
+	const float distToL = length(vToL);
 	const float3 dirToL = vToL / distToL;
 	//  Attenuation
 	const float att = 1.0f / (attConst + attLin * distToL + attQuad * (distToL * distToL));
@@ -32,5 +37,5 @@ float4 main(float3 worldPos : Position, float3 n : Normal) : SV_Target
 	// Calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
 	const float3 specular = att * (diffuseColor * diffuseIntensity) * specularIntensity * pow(max(0.0f, dot(normalize(-r), normalize(worldPos))), specularPower );
 	// Final color
-	return float4(saturate((diffuse + ambient + specular) * materialColor), 1.0f);
+	return float4(saturate(diffuse + ambient + specular), 1.0f) * tex.Sample(splr, tc);
 }
